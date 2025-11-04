87a7fed7cdc2120f81194da766e9b3abd7f02652
Divine Orji <dpkreativ@gmail.com>
2025-11-04 12:42:24 +0100

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Button>Click Issac 🌚</Button>
-      </main>
-    </div>
+    <main>
+      <img src="/logo.svg" alt="cura logo" />
+      <h1>Cura</h1>
+    </main>
   );
 }
